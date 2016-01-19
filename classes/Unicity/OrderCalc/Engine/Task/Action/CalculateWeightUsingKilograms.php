fbf5fb4b6e3a768dbfd226c6fe2a1a32c4291f93
Matthew A. Mattson <matthew.mattson@unicity.com>
2016-01-19 14:10:23 -0700

    Adding two new actions for calculating weight.

---
+++ classes/Unicity/OrderCalc/Engine/Task/Action/CalculateWeightUsingKilograms.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright 2015-2016 Unicity International
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Unicity\OrderCalc\Engine\Task\Action {
+
+	use \Unicity\BT;
+	use \Unicity\Core;
+	use \Unicity\Trade;
+	use \Unicity\OrderCalc;
+
+	class CalculateWeightUsingKilograms extends BT\Task\Action {
+
+		/**
+		 * This constant represents the conversion rate for converting pounds to kilograms.
+		 *
+		 * @access public
+		 * @const double
+		 */
+		const LBS_TO_KGS_CONVERSION_RATE = 2.2046;
+
+		/**
+		 * This method processes the models and returns the status.
+		 *
+		 * @access public
+		 * @param BT\Exchange $exchange                             the exchange given to process
+		 * @return integer                                          the status code
+		 */
+		public function process(BT\Exchange $exchange) {
+			$order = $exchange->getIn()->getBody()->Order;
+
+			$weight = 0.0;
+
+			foreach ($order->lines->items as $line) {
+				$value = $line->item->weightEach->value;
+				$uom = $line->item->weightEach->unit;
+				if (preg_match('/^lb(s)?$/i', $uom)) {
+					$value = $value / self::LBS_TO_KGS_CONVERSION_RATE;
+				}
+				else if (!preg_match('/^kg(s)?$/i', $uom)) {
+					return BT\Task\Status::ERROR;
+				}
+				$weight += $line->quantity * $value;
+			}
+
+			$order->lines->aggregate->weight->value = 'kg';
+			$order->lines->aggregate->weight->value = $weight;
+
+			return BT\Task\Status::SUCCESS;
+		}
+
+	}
+
+}
