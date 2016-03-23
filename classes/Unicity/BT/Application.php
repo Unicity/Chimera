fbee190ae787d4ff2c0128741b1ac8e7f1a46521
Matthew A. Mattson <matthew.mattson@unicity.com>
2016-03-23 15:41:52 -0600

    Refactoring BTs to use the "Enitity-Component-Systems" design pattern (part one).

---
+++ classes/Unicity/BT/Application.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Copyright 2015-2016 Unicity International
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Unicity\BT {
+
+	use \Unicity\BT;
+	use \Unicity\Common;
+	use \Unicity\Core;
+	use Unicity\MappingService\Data\Model\Entity;
+
+	/**
+	 * This class represents an exchange.
+	 *
+	 * @access public
+	 * @class
+	 * @see http://www.gamedev.net/page/resources/_/technical/game-programming/understanding-component-entity-systems-r3013
+	 * @see http://www.gamedev.net/page/resources/_/technical/game-programming/implementing-component-entity-systems-r3382
+	 */
+	class Application extends Core\Object {
+
+		/**
+		 * This variable stores a map of id/entity pairs.
+		 *
+		 * @access protected
+		 * @var array
+		 */
+		protected $entities;
+
+		/**
+		 * This constructor initializes the class.
+		 *
+		 * @access public
+		 */
+		public function __construct() {
+			$this->entities = new Common\Mutable\HashMap();
+		}
+
+		/**
+		 * This destructor ensures that any resources are properly disposed.
+		 *
+		 * @access public
+		 */
+		public function __destruct() {
+			parent::__destruct();
+			unset($this->entities);
+		}
+
+		/**
+		 * This method returns the value associated with the specified key.
+		 *
+		 * @access public
+		 * @param string $entityId                                  the id of the entity
+		 * @return BT\Entity                                        the entity
+		 */
+		public function getEntity($entityId) {
+			return $this->entities->getValue($entityId);
+		}
+
+		/**
+		 * This method returns an array list of entities.
+		 *
+		 * @access public
+		 * @return Common\ArrayList                                 an array list of entities
+		 */
+		public function getEntities() {
+			return new Common\ArrayList($this->entities->getValues());
+		}
+
+		/**
+		 * This method creates an entity id.
+		 *
+		 * @access public
+		 * @static
+		 * @param Application $application                          the application for which the entity
+		 *                                                          is being created
+		 * @return entity                                           the entity id
+		 */
+		public static function createEntity(BT\Application $application) {
+			$entityId = 0;
+			while ($application->entities->hasKey($entityId)) {
+				$entityId++;
+			}
+			$entity = new Entity($entityId);
+			$application->entities->putEntry($entityId, $entity);
+			return $entityId;
+		}
+
+		/**
+		 * This method removes an entity id.
+		 *
+		 * @access public
+		 * @static
+		 * @param Application $application
+		 * @param integer $entityId
+		 */
+		public static function destroyEntity(BT\Application $application, $entityId) {
+			unset($application->entities[$entityId]);
+		}
+
+	}
+
+}
--- classes/Unicity/BT/Entity.php
+++ classes/Unicity/BT/Entity.php
@@ -18,98 +18,29 @@
 
 namespace Unicity\BT {
 
+	use \Unicity\BT;
 	use \Unicity\Common;
 	use \Unicity\Core;
-	use \Unicity\IO;
-	use \Unicity\Throwable;
 
 	/**
 	 * This class represents an entity.
 	 *
 	 * @access public
 	 * @class
-	 * @see http://camel.apache.org/maven/current/camel-core/apidocs/org/apache/camel/Message.html
+	 * @see http://docs.unity3d.com/ScriptReference/GameObject.html
 	 */
-	class Entity extends Core\Object implements Core\IMessage {
+	class Entity extends Core\Object {
 
 		/**
-		 * This variable stores the HTTP status codes and descriptions.
-		 *
-		 * @access protected
-		 * @var array
-		 */
-		protected static $statuses = array(
-			// Informational 1xx
-			100 => 'Continue',
-			101 => 'Switching Protocols',
-
-			// Success 2xx
-			200 => 'OK',
-			201 => 'Created',
-			202 => 'Accepted',
-			203 => 'Non-Authoritative Information',
-			204 => 'No Content',
-			205 => 'Reset Content',
-			206 => 'Partial Content',
-
-			// Redirection 3xx
-			300 => 'Multiple Choices',
-			301 => 'Moved Permanently',
-			302 => 'Found', // 1.1
-			303 => 'See Other',
-			304 => 'Not Modified',
-			305 => 'Use Proxy',
-			// 306 is deprecated but reserved
-			307 => 'Temporary Redirect',
-
-			// Client Error 4xx
-			400 => 'Bad Request',
-			401 => 'Unauthorized',
-			402 => 'Payment Required',
-			403 => 'Forbidden',
-			404 => 'Not Found',
-			405 => 'Method Not Allowed',
-			406 => 'Not Acceptable',
-			407 => 'Proxy Authentication Required',
-			408 => 'Request Timeout',
-			409 => 'Conflict',
-			410 => 'Gone',
-			411 => 'Length Required',
-			412 => 'Precondition Failed',
-			413 => 'Request Entity Too Large',
-			414 => 'Request-URI Too Long',
-			415 => 'Unsupported Media Type',
-			416 => 'Requested Range Not Satisfiable',
-			417 => 'Expectation Failed',
-
-			// Server Error 5xx
-			500 => 'Internal Server Error',
-			501 => 'Not Implemented',
-			502 => 'Bad Gateway',
-			503 => 'Service Unavailable',
-			504 => 'Gateway Timeout',
-			505 => 'HTTP Version Not Supported',
-			509 => 'Bandwidth Limit Exceeded'
-		);
-
-		/**
-		 * This variable stores a reference to the body of the message.
-		 *
-		 * @access protected
-		 * @var mixed
-		 */
-		protected $body;
-
-		/**
-		 * This variable stores the message's headers.
+		 * This variable stores the components associated with this entity.
 		 *
 		 * @access protected
 		 * @var Common\Mutable\HashMap
 		 */
-		protected $headers;
+		protected $component;
 
 		/**
-		 * This variable stores the message's id.
+		 * This variables stores the components associated with this entity.
 		 *
 		 * @access protected
 		 * @var integer
@@ -117,38 +48,13 @@ namespace Unicity\BT {
 		protected $id;
 
 		/**
-		 * This variable stores the HTTP protocol.
-		 *
-		 * @access protected
-		 * @var string
-		 */
-		protected $protocol;
-
-		/**
-		 * This variable stores the HTTP status code.
-		 *
-		 * @access protected
-		 * @var integer
-		 */
-		protected $status;
-
-		/**
 		 * This constructor initializes the class.
 		 *
 		 * @access public
-		 * @param mixed $body                                       the body of the message
 		 */
-		public function __construct($body = null) {
-			$this->headers = new Common\Mutable\HashMap();
-			$this->headers->putEntry('content-disposition', 'inline');
-			$this->headers->putEntry('content-type', 'text/plain; charset=UTF-8');
-			$this->headers->putEntry('cache-control', 'no-store, no-cache, must-revalidate');
-			$this->headers->putEntry('expires', 'Sat, 26 Jul 1997 05:00:00 GMT');
-			$this->headers->putEntry('pragma', 'no-cache');
-			$this->body = $body;
-			$this->id = $this->__hashCode();
-			$this->protocol = 'HTTP/1.1';
-			$this->status = 200;
+		public function __construct($id) {
+			$this->component = new Common\Mutable\HashMap();
+			$this->id = $id;
 		}
 
 		/**
@@ -158,202 +64,81 @@ namespace Unicity\BT {
 		 */
 		public function __destruct() {
 			parent::__destruct();
-			unset($this->body);
-			unset($this->headers);
+			unset($this->component);
 			unset($this->id);
-			unset($this->protocol);
-			unset($this->status);
 		}
 
 		/**
-		 * This method returns the body of the message.
+		 * This method returns whether this entity has a component with the specified name.
 		 *
 		 * @access public
-		 * @return mixed                                            the body of the message
+		 * @param string $name                                      the name of the component
+		 * @return boolean                                          whether this entity has the component
 		 */
-		public function getBody() {
-			return $this->body;
+		public function hasComponent($name) {
+			return $this->component->hasKey($name);
 		}
 
 		/**
-		 * This method returns the message's length.
+		 * This method returns whether this entity has all components with the specified names.
 		 *
 		 * @access public
-		 * @return integer                                          the message's length
+		 * @param array $names                                      the names of the components
+		 * @return boolean                                          whether this entity has all of the
+		 *                                                          components
 		 */
-		public function getLength() {
-			if ($this->body !== null) {
-				if ($this->body instanceof IO\File) {
-					return $this->body->getFileSize();
+		public function hasComponents(array $names) {
+			if (count($names) > 0) {
+				foreach ($names as $name) {
+					if (!$this->component->hasKey($name)) {
+						return false;
+					}
 				}
-				return strlen(Core\Convert::toString($this->body));
+				return true;
 			}
-			return 0;
+			return false;
 		}
 
 		/**
-		 * This method returns the message header mapped to the given name.
+		 * This method returns the component associated with the specified name.
 		 *
 		 * @access public
-		 * @param string $name                                      the name of the header
-		 * @return string                                           the value of the header
+		 * @param string $name                                      the name of the component
+		 * @return Common\Mutable\ICollection                       the component
 		 */
-		public function getHeader($name) {
-			$name = strtolower($name);
-			if ($this->headers->hasKey($name)) {
-				return $this->headers->getValue($name);
-			}
-			return '';
+		public function getComponent($name) {
+			return $this->component->getValue($name);
 		}
 
 		/**
-		 * This method returns the message's id.
+		 * This method removes the component associated with the specified name.
 		 *
 		 * @access public
-		 * @return string                                           the message's id
+		 * @param string $name                                      the name of the component
 		 */
-		public function getMessageId() {
-			return $this->id;
+		public function removeComponent($name) {
+			$this->component->removeKey($name);
 		}
 
 		/**
-		 * This method returns the HTTP protocol.
+		 * This method removes all components associated with the specified names.
 		 *
 		 * @access public
-		 * @return string                                           the HTTP protocol
+		 * @param array $names                                      the names of the components
 		 */
-		public function getProtocol() {
-			return $this->protocol;
+		public function removeComponents(array $names) {
+			$this->component->removeKeys($names);
 		}
 
 		/**
-		 * This method returns the HTTP status code.
+		 * This method sets the component with the specified name.
 		 *
 		 * @access public
-		 * @return integer                                          the HTTP status code
+		 * @param string $name                                      the name of the component
+		 * @param Common\Mutable\ICollection $component             the component to be set
 		 */
-		public function getStatus() {
-			return $this->status;
-		}
-
-		/**
-		 * This method sets the body with the contents in the standard input stream
-		 * buffer.
-		 *
-		 * @access public
-		 * @return IO\File                                          the message's body
-		 */
-		public function receive() {
-			$this->body = new IO\InputBuffer();
-			return $this->body;
-		}
-
-		/**
-		 * This method sends the message.
-		 *
-		 * @access public
-		 */
-		public function send() {
-			if ($this->body instanceof IO\File) {
-				$body = $this->body->getBytes();
-			}
-			else {
-				$body = Core\Convert::toString($this->body);
-			}
-
-			header(implode(' ', array($this->protocol, $this->status, static::$statuses[$this->status])));
-
-			foreach ($this->headers as $name => $value) {
-				header($name . ': ' . trim($value));
-			}
-
-			header('content-length: ' . strlen($body));
-
-			echo $body;
-
-			exit();
-		}
-
-		/**
-		 * This method sets the body of the message.
-		 *
-		 * @access public
-		 * @param mixed $body                                       the body of the message
-		 */
-		public function setBody($body = null) {
-			$this->body = $body;
-		}
-
-		/**
-		 * This method sets the header with specified name.
-		 *
-		 * @access public
-		 * @param string $name                                      the name of the header
-		 * @param string $value                                     the value of the header
-		 */
-		public function setHeader($name, $value) {
-			$name = strtolower($name);
-			if (!in_array($name, array('content-length'))) {
-				if ($value !== null) {
-					$this->headers->putEntry($name, Core\Convert::toString($value));
-				}
-				else {
-					$this->headers->removeKey($name);
-				}
-			}
-		}
-
-		/**
-		 * This method sets the headers with the specified name/value pairs.
-		 *
-		 * @access public
-		 * @param array $headers                                    the headers associated with the message
-		 */
-		public function setHeaders(array $headers) {
-			foreach ($headers as $name => $value) {
-				$this->setHeader($name, $value);
-			}
-		}
-
-		/**
-		 * This method sets the message's id.
-		 *
-		 * @access public
-		 * @param string $id
-		 * @throws Throwable\Parse\Exception                        the message id
-		 */
-		public function setMessageId($id) {
-			if ($this->id !== null) {
-				$this->id = Core\Convert::toString($id);
-			}
-			else {
-				$this->id = $this->__hashCode();
-			}
-		}
-
-		/**
-		 * This method sets the HTTP protocol.
-		 *
-		 * @access public
-		 * @param string $protocol                                  the HTTP protocol to be set
-		 */
-		public function setProtocol($protocol) {
-			$this->protocol = strtoupper($protocol);
-		}
-
-		/**
-		 * This method sets the HTTP status code.
-		 *
-		 * @access public
-		 * @param integer $status                                   the HTTP status code to be set
-		 * @throws Throwable\InvalidArgument\Exception              indicates the specified status
-		 *                                                          code is not known
-		 */
-		public function setStatus($status) {
-			if (!isset(static::$statuses[$status])) {
-				throw new Throwable\InvalidArgument\Exception('Invalid status code. Expected an HTTP status code, but got ":status".', array(':status' => $status));
-			}
-			$this->status = (int) $status;
+		public function setComponent($name, Common\Mutable\ICollection $component) {
+			$this->component->putEntry($name, $component);
 		}
 
 	}
