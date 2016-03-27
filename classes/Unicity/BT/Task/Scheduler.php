2b17def9016282b9b1857b42df995c723ad1f733
Matthew A. Mattson <matthew.mattson@unicity.com>
2016-03-26 20:56:37 -0600

    Adding task scheduler class.

---
+++ classes/Unicity/BT/Task/Scheduler.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright 2015-2016 Unicity International
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace Unicity\BT\Task {
+
+	use \Unicity\BT;
+
+	/**
+	 * This class represents a task scheduler.
+	 *
+	 * @access public
+	 * @class
+	 */
+	class Scheduler extends BT\Task\Action {
+
+		/**
+		 * This method processes an entity.
+		 *
+		 * @access public
+		 * @param string $entityId                                  the entity id being processed
+		 * @param BT\Engine $engine                                 the engine
+		 * @return integer                                          the status
+		 */
+		public function process(string $entityId, BT\Engine $engine) {
+			$engine->getEntity($entityId)->setTaskId($this->policy->getValue('task'));
+			return BT\Status::ACTIVE;
+		}
+
+	}
+
+}
