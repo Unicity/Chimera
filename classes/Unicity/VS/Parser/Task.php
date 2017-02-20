11bcaa137e954cadf66ecdb168da36dff11a0e95
Matthew A. Mattson <matthew.mattson@unicity.com>
2017-02-20 09:18:14 -0700

    Adding missing license.

--- classes/Unicity/VS/Parser/Task.php
+++ classes/Unicity/VS/Parser/Task.php
@@ -20,20 +20,8 @@ declare(strict_types = 1);
 
 namespace Unicity\VS\Parser {
 
-	use \Unicity\VS;
+	use \Unicity\Common;
 
-	class RunStatement implements VS\Parser\Statement {
-
-		protected $args;
-
-		public function __construct(array $args) {
-			$this->args = $args;
-		}
-
-		public function accept0() : void {
-			call_user_func_array($this->args[0]->get0(), [$this->args[1]->get0(), $this->args[2]->get0()]);
-		}
-
-	}
+	interface Task extends Common\IBiPredicate { }
 
 }
