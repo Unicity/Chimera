d44ca74069711056277c7cc5ca11379992453189
Matthew A. Mattson <matthew.mattson@unicity.com>
2017-02-19 22:43:29 -0700

    Adding a simple symbol table.

---
+++ classes/Unicity/VS/Automaton/SymbolTable.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Unicity\VS\Automaton {
+
+	use \Unicity\Common;
+	use \Unicity\Core;
+
+	class SymbolTable extends Core\Object {
+
+		protected static $table = null;
+
+		public static function instance() : Common\Mutable\HashMap {
+			if (static::$table === null) {
+				static::$table = new Common\Mutable\HashMap();
+			}
+			return static::$table;
+		}
+
+	}
+
+}
