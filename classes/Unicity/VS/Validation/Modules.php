b4a48821f2627bf4531b657d762e5edf61842169
Matthew A. Mattson <matthew.mattson@unicity.com>
2017-02-23 00:23:04 -0700

    Adding some basic modules.

--- classes/Unicity/VS/Validation/Modules.php
+++ classes/Unicity/VS/Validation/Modules.php
@@ -2,4 +2,11 @@
 
 return array(
 	'schema' => '\\Unicity\\VS\\Validation\\Module\\HasSchema',
+
+	'eq' => '\\Unicity\\VS\\Validation\\Module\\IsEqualTo',
+	'gt' => '\\Unicity\\VS\\Validation\\Module\\IsGreaterThan',
+	'ge' => '\\Unicity\\VS\\Validation\\Module\\IsGreaterThanOrEqualTo',
+	'lt' => '\\Unicity\\VS\\Validation\\Module\\IsLesserThan',
+	'le' => '\\Unicity\\VS\\Validation\\Module\\IsLesserThanOrEqualTo',
+	'ne' => '\\Unicity\\VS\\Validation\\Module\\IsNotEqualTo',
 );
