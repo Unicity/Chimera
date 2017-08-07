87b05fc22328d35975f2e1338a37b08007a7cd12
Matthew A. Mattson <matthew.mattson@unicity.com>
2017-08-07 09:35:04 -0600

    Creating two new message classes.

---
+++ classes/Unicity/HTTP/ResponseMessage.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Copyright 2015-2016 Unicity International
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+namespace Unicity\HTTP {
+
+	use \Unicity\EVT;
+
+	class ResponseMessage extends EVT\Message {
+
+		/**
+		 * This variable stores the HTTP status codes and descriptions.
+		 *
+		 * @access protected
+		 * @var array
+		 */
+		protected static $statuses = array(
+			// Informational 1xx
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+
+			// Success 2xx
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+
+			// Redirection 3xx
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found', // 1.1
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			// 306 is deprecated but reserved
+			307 => 'Temporary Redirect',
+
+			// Client Error 4xx
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+
+			// Server Error 5xx
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported',
+			509 => 'Bandwidth Limit Exceeded'
+		);
+
+		public function __construct(array $map = []) {
+			parent::__construct(array_merge($map, [
+				'body' => '',
+				'headers' => [],
+				'protocol' => 'HTTP/1.1',
+				'status' => 200,
+				'statusText' => 'OK',
+			]));
+		}
+
+		public static function getStatusText(int $status) {
+			return static::$statuses[$status];
+		}
+
+	}
+
+}
