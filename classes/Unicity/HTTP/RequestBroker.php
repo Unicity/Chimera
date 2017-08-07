b0dec79d85ea9d7192784cc95ab388928bc0a39c
Matthew A. Mattson <matthew.mattson@unicity.com>
2017-08-07 14:16:55 -0600

    Refactored to use helper method instead of constructor.

--- classes/Unicity/HTTP/RequestBroker.php
+++ classes/Unicity/HTTP/RequestBroker.php
@@ -161,7 +161,7 @@ namespace Unicity\HTTP {
 					$error = curl_error($resource);
 					@curl_close($resource);
 					$status = 503;
-					$response = new HTTP\ResponseMessage([
+					$response = HTTP\RequestMessage::factory([
 						'body' => $error,
 						'headers' => [
 							'http_code' => $status,
@@ -178,7 +178,7 @@ namespace Unicity\HTTP {
 					$headers = curl_getinfo($resource);
 					@curl_close($resource);
 					$status = $headers['http_code'];
-					$response = new HTTP\ResponseMessage([
+					$response = HTTP\RequestMessage::factory([
 						'body' => $body,
 						'headers' => $headers,
 						'status' => $status,
@@ -199,7 +199,7 @@ namespace Unicity\HTTP {
 			}
 			else {
 				$status = 503;
-				$response = new HTTP\ResponseMessage([
+				$response = HTTP\RequestMessage::factory([
 					'body' => 'Failed to create cURL resource.',
 					'headers' => [
 						'http_code' => $status,
