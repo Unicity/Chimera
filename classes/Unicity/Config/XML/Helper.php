bc2c4baf2d607ede4712c4b451dfe091f8e093fe
Matthew A. Mattson <matthew.mattson@unicity.com>
2018-02-16 17:05:35 -0700

    Updated XML helper.

--- classes/Unicity/Config/XML/Helper.php
+++ classes/Unicity/Config/XML/Helper.php
@@ -28,26 +28,47 @@ namespace Unicity\Config\XML {
 
 	class Helper extends Core\Object {
 
-		public static function decode($data) {
-			if ($data instanceof Common\ICollection) {
-				return $data;
+		public static function decode($data, array $metadata = array(), bool $assoc = false) /* array|object */{
+			$buffer = static::unmarshal($data, $metadata);
+			if ($assoc) {
+				return Common\Collection::useArrays($buffer);
 			}
-			if (Common\StringRef::isTypeOf($data)) {
-				$data = new IO\StringRef(Core\Convert::toString($data));
-			}
-			return MappingService\Data\Model\Marshaller::unmarshal(
-				Config\XML\Reader::load($data)
-			);
+			return Common\Collection::useObjects($buffer);
 		}
 
-		public static function encode($collection) : string {
+		public static function encode($collection, array $metadata = array()) : string {
+			if ($collection instanceof \JsonSerializable) {
+				$collection = json_decode(json_encode($collection));
+			}
 			if ($collection instanceof IO\FIle) {
 				return $collection->getBytes();
 			}
 			if (Common\StringRef::isTypeOf($collection)) {
 				return Core\Convert::toString($collection);
 			}
-			return (new Config\XML\Writer($collection))->render();
+			return (new Config\XML\Writer($collection))->config($metadata)->render();
+		}
+
+		public static function marshal($collection, array $metadata = array()) : IO\File {
+			return new IO\StringRef(static::encode($collection, $metadata));
+		}
+
+		public static function unmarshal($data, array $metadata = array()) /* list|map */{
+			if (is_array($data) || ($data instanceof \JsonSerializable)) {
+				$data = new IO\StringRef(static::encode($data));
+			}
+			if ($data instanceof Common\ICollection) {
+				if (isset($metadata['encoding'])) {
+					$data = \Unicity\Core\Data\Charset::encodeData($data, $metadata['encoding'][0], $metadata['encoding'][0]);
+				}
+				return $data;
+			}
+			if (Common\StringRef::isTypeOf($data)) {
+				$data = new IO\StringRef(Core\Convert::toString($data));
+			}
+			return MappingService\Data\Model\Marshaller::unmarshal(
+				Config\XML\Reader::load($data, $metadata)
+			);
 		}
 
 	}
