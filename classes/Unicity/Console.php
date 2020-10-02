994f3ebe626b10adca6ebbffa55fd5928cb1562c
Matthew Mattson <matthew.mattson@unicity.com>
2020-10-02 15:12:57 -0600

    Adding method to write to Console logs.

---
+++ classes/Unicity/Console.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Unicity {
+
+	final class Console {
+
+		public static function log($message) {
+			fwrite(fopen("php://stdout", "w"), strval($message));
+		}
+
+	}
+
+}
