6fb0b257d32046722528b0b6f24c618bbc1eff51
Luke Fairchild <luke.fairchild@unicity.com>
2024-03-26 14:12:49 -0600

    missed the comment type

--- classes/Unicity/HTTP/RequestBroker.php
+++ classes/Unicity/HTTP/RequestBroker.php
@@ -57,7 +57,6 @@ namespace Unicity\HTTP {
 		 * This method executes the given request.
 		 *
 		 * @access public
-		 * @param HTTP\Request $request                             the request to be sent
 		 * @return int                                              the response status
 		 */
 		public function execute($request) {
